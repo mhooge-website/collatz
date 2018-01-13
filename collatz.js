be4f22d8d0b4925ce27571728853ce72fe484322
Mikkel <mhso@itu.dk>
2018-01-13 19:15:19 +0100

    COllatz fun

--- collatz.js
+++ collatz.js
@@ -1,11 +1,17 @@
+var vertices;
+
 function calculate(startPoint) {
+    vertices = new Array(number/10);
     var progress = startPoint;
     var counter = 0;
     while(progress > 1) {
+        vertices.push(progress);
         if(progress % 2 == 1) progress = (progress * 3) + 1;
         else progress /= 2;
         if(counter % 1000 == 0) console.log(progress);
         counter++;
     }
+
     document.write(startPoint + " reached " + progress + " in " + counter + " steps.");
+    drawGraph(vertices);
 }
---
+++ graph.js
@@ -0,0 +1,5 @@
+var vertices;
+
+function addPoint(value) {
+    vertices.push(value);
+}
